5b6c531d2b081288bcb1fdfbfcbd312c23e9580d
AMN NAYEEF <nayeefamn@gmail.com>
2025-03-23 15:17:27 +0600

    function

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
 
 // reserved keywords
 // const interface = 'audio';
 // const private= 534;
+
+
+function logger() {
+    console.log('My name is Nayeef');
+}
+
+//calling / running / invoking function
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const mixJuice = fruitProcessor(5, 3);
+console.log(mixJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
