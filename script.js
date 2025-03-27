5e7dd186cf7d7aa9a04dc51741a202d12ab315eb
AMN NAYEEF <nayeefamn@gmail.com>
2025-03-28 03:07:42 +0600

    object

--- script.js
+++ script.js
@@ -170,6 +170,64 @@
 // }
 
 
+// CHALLENGE #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+// Each team competes 3 times, and then the average of the 3 scores is calculated(so one average score per team).
+
+// A team only wins if it has at least double the average score of the other team.Otherwise, no team wins!
+
+
+
+// Your tasks:
+
+// Create an arrow function calcAverage to calculate the average of 3 scores.This function should have three parameters and return a single number(the average score).
+
+// Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them(you will need to call this function, and pass scores as arguments).
+
+// Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above.Example: Koalas win(30 vs. 13)(use avgDolphins and avgKoalas instead of hard - coded values).
+
+// Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+
+// Ignore draws this time.Instead, log No team wins... to the console if there is no winner.
+
+
+
+// TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+// TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
+
+// //Solution
+// const calcAverage = (x, y, z) => (x+y+z)/3;
+
+
+// let scoreDolphins =calcAverage(44, 23, 71);
+// let scoreKoalas =calcAverage(65, 54, 49);
+
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function(avgDolphins, avgKoalas){
+//     if(avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+//     }else if(avgKoalas >= 2 * avgDolphins){
+//         console.log(`koalas win (${avgKoalas} vs ${avgDolphins})`);
+//     }else{
+//         console.log(`No team wins....`)
+
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
+//test 2
+// scoreDolphins =calcAverage(85, 54, 41);
+// scoreKoalas =calcAverage(23, 34, 27);
+
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins,scoreKoalas);
+
+
+
 // CHALLENGE #2
 // Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
@@ -200,3 +258,19 @@
 
 // const totals = [(bills[0] + calcTip(bills[0])), (bills[1] + calcTip(bills[1])), (bills[2] + calcTip(bills[2]))]
 // console.log(totals);
+
+const nayeefArray = [
+    'Nayeef',
+    'Nowfal',
+    2025 - 1998,
+    'Student',
+    ['Talha', 'Ashfi', 'Eraz']
+];
+
+const nayeef = {
+    firstName= 'Nayeef',
+    lastName= 'Nowfal',
+    age = 2025 - 1998,
+    job = 'Student',
+    friends =['Talha', 'Ashfi', 'Eraz']
+};
